58f6b08a849c402e6ea95bc1b4da6000b8a797df
Andriansyah Rezi Yazidan <116085737+Yazidan@users.noreply.github.com>
2022-11-02 08:44:09 +0700

    Add files via upload

---
+++ projectuts1AndriansyahReziYazidanF5A5.dart
@@ -0,0 +1,14 @@
+void main(){
+    // membuat variabel dengan tipe data
+    String nama = "Andriansyah Rezi Yazidan";
+    int nilai = 70;
+    String kelas = "F5A5";
+    String matakuliah = "Pemrograman Perangkat Bergerak";
+
+
+    // mencetak variabel
+    print("Nama saya $nama.");
+    print("Nilai UTS $nilai.");
+    print("Kelas $kelas.");
+    print("Mata Kuliah $matakuliah. ");
+}
